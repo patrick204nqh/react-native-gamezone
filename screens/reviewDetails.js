0f1e63232039bb2ca9d47c53a3532fb50aee0a06
patrick <patrick204nqh@gmail.com>
2020-05-30 12:48:29 +0700

    usign images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({ navigation }) {
-  const pressHandler = () => {
-    navigation.goBack();
-  };
+  const rating = navigation.getParam('rating');
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{navigation.getParam('title')}</Text>
         <Text>{navigation.getParam('body')}</Text>
-        <Text>{navigation.getParam('rating')}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  },
+});
