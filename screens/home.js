accca8882e50f22112cb480c4c0682078975418c
patrick <patrick204nqh@gmail.com>
2020-05-30 13:28:50 +0700

    modals

--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  StyleSheet,
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
 
 export default function Home({ navigation }) {
+  const [modalOpen, setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
     {
       title: 'Zelda, Breath of Fresh Air',
@@ -27,6 +36,25 @@ export default function Home({ navigation }) {
 
   return (
     <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType='slide'>
+        <View style={styles.modalContent}>
+          <MaterialIcons
+            name='close'
+            size={24}
+            style={{ ...styles.modalToggle, ...styles.modalClose }}
+            onPress={() => setModalOpen(false)}
+          ></MaterialIcons>
+          <Text>Hello from the modal</Text>
+        </View>
+      </Modal>
+
+      <MaterialIcons
+        name='add'
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)}
+      ></MaterialIcons>
+
       <FlatList
         data={reviews}
         renderItem={({ item }) => (
@@ -42,3 +70,21 @@ export default function Home({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    padding: 1,
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalContent: {
+    flex: 1,
+  },
+});
