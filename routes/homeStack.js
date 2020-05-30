886474f00c00f4b6edaa524ba0e5221c6cf6483c
patrick <patrick204nqh@gmail.com>
2020-05-30 11:34:29 +0700

    drawer navigation

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,4 +1,3 @@
-import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails';
@@ -25,4 +24,4 @@ const HomeStack = createStackNavigator(screens, {
   },
 });
 
-export default createAppContainer(HomeStack);
+export default HomeStack;
